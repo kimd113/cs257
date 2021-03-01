d0b5b452bb3190401bb82f2dbff6d104e384743b
kimd113 <kimd@carleton.edu>
2021-03-02 08:53:46 +0900

    Fix onSearchButton

--- webapp/static/webapp.js
+++ webapp/static/webapp.js
@@ -28,6 +28,11 @@ function initialize() {
     if (searchButton) {
         searchButton.onclick = onSearchButton;
     }
+
+    let signUpButton = document.getElementById('signup');
+    if (signUpButton) {
+        signUpButton.onclick = onSignUpButton;
+    }
 }
 
 function getAPIBaseURL() {
@@ -88,40 +93,50 @@ function onSearchButton() {
     /**
      * When user enters the main page, send request to the server and get the list of trending videos.
      */
-    console.log("111");
-    let day = document.getElementById('search_trending_time_input_day');
-    let month = document.getElementById('search_trending_time_input_month');
-    let year = document.getElementById('search_trending_time_input_year');
-
-    date = day.value + '.' +  month.value + '.' + year.value 
-    let url =  getAPIBaseURL() + '?trending_date=' + date;
-
+    let day = document.getElementById('input_date');
+    let month = document.getElementById('input_month');
+    let year = document.getElementById('input_year');
+    
+    let date = year.value + '.' +  day.value + '.' + month.value;
+    let url =  `${getAPIBaseURL()}?trending_date=${date}`;
+    
     fetch(url, {method: 'get'})
     .then((response) => response.json())
     .then((videos) => {
         console.log(videos);
         videos_list = videos;
-        let listBody = '';
+        let listBodyFirstRow = '';
+        let listBodySecondRow = '';
+        videos_list_current_page = 0;
         for (let i = 0; i < 10; i++) {
             let video = videos[i];
-            listBody += `<li class="now_trending_videos_list_item">
+            let listBody = `<div class="now_trending_videos_list_item col card">
                 <a href="https://www.youtube.com/watch?v=${video.link}" target="_blank">
-                    <img class="video_img" src=${video.thumbnail_link} alt="" />
+                    <img class="video_img card-img-top" src=${video.thumbnail_link} alt="" />
                     <div class="video_title">${video.title}</div>
+                </a>
                     <div class="video_channel">${video.channel}</div>
                     <div class="video_list_item_text">
                         <span class="video_views">${video.views} </span>|
                         <span class="video_likes">${video.likes} </span>|
                         <span class="video_dislikes">${video.dislikes}</span>
                     </div>
-                    <button class="save_to_playlist_button">Save to my playlist</button>
-                </a>
-            </li>`;
+                    <button class="save_to_playlist_button btn btn-sm btn-outline-primary">Save to my playlist</button>
+                </div>`;
+            if (i < 5) {
+                listBodyFirstRow += listBody;
+            } else {
+                listBodySecondRow += listBody;
+            }
         }
 
-        let videosListElement = document.getElementById('now_trending_videos_list');
-        if (videosListElement) {
-            videosListElement.innerHTML = listBody;
+        let videosListElementFirstRow = document.getElementById('now_trending_videos_list_1');
+        let videosListElementSecondRow = document.getElementById('now_trending_videos_list_2');
+        if (videosListElementFirstRow) {
+            videosListElementFirstRow.innerHTML = listBodyFirstRow;
+        }
+        if (videosListElementSecondRow) {
+            videosListElementSecondRow.innerHTML = listBodySecondRow;
         }
     })
     .catch(function(error) {
@@ -233,37 +248,6 @@ function onSignUpButton() {
     /**
      * When user clicks the sign up button, create an account for the user*
      */
-    if (videos_list_current_page == Math.ceil(videos_list.length / 10) - 1) {
-        alert('This is the last page of videos.');
-        return;
-    }
-    let videosListElement = document.getElementById('now_trending_videos_list');
-    videosListElement.innerHTML = '';
-
-    
-    videos_list_current_page += 1;
-    let next_page_count = videos_list_current_page * 10;
-    let listBody = '';
-    for (let i = next_page_count; i < next_page_count + 10; i++) {
-        if (videos_list[i]) {
-            let video = videos_list[i];
-            listBody += `<li class="now_trending_videos_list_item">
-                <a href="https://www.youtube.com/watch?v=${video.link}" target="_blank">
-                    <img class="video_img" src=${video.thumbnail_link} alt="" />
-                    <div class="video_title">${video.title}</div>
-                    <div class="video_channel">${video.channel}</div>
-                    <div class="video_list_item_text">
-                        <span class="video_views">${video.views} </span>|
-                        <span class="video_likes">${video.likes} </span>|
-                        <span class="video_dislikes">${video.dislikes}</span>
-                    </div>
-                    <button class="save_to_playlist_button">Save to my playlist</button>
-                </a>
-            </li>`;
-        }
-    }
-
-    if (videosListElement) {
-        videosListElement.innerHTML = listBody;
-    }
+    getVideosListInMainPage();
+    console.log('hi');
 }
