527276601eb2f137253c351564d98503a437f8fd
kimd113 <kimd@carleton.edu>
2021-03-04 10:02:57 +0900

    Add onClickVideoPage

--- webapp/static/webapp.js
+++ webapp/static/webapp.js
@@ -44,6 +44,14 @@ function initialize() {
     if (verticalButton) {
         verticalButton.onclick = onVerticalViewButton;
     }
+
+    // let pageItem = document.querySelectorAll(".page-item");
+    // if (pageItem) {
+    //     pageItem.forEach(function(element) {
+    //         element.onclick = onClickVideoPage;
+    //     });
+    // }
+
 }
 
 function getAPIBaseURL() {
@@ -51,21 +59,13 @@ function getAPIBaseURL() {
     return baseURL;
 }
 
-function getVideosListInMainPage() {
-    /**
-     * When user enters the main page, send request to the server and get the list of trending videos.
-     */
-    let url =  getAPIBaseURL();
+function renderHorizontalVideosList(page_count) {
+    let listBodyFirstRow = '';
+    let listBodySecondRow = '';
 
-    fetch(url, {method: 'get'})
-    .then((response) => response.json())
-    .then((videos) => {
-        videos_list = videos;
-        videos_list_total_number = videos_list.length;
-        let listBodyFirstRow = '';
-        let listBodySecondRow = '';
-        for (let i = 0; i < 10; i++) {
-            let video = videos[i];
+    for (let i = page_count; i < page_count + 10; i++) {
+        if (videos_list[i]) {
+            let video = videos_list[i];
             let listBody = `<div class="now_trending_videos_list_item col card">
                 <a href="https://www.youtube.com/watch?v=${video.link}" target="_blank">
                     <img class="video_img card-img-top" src=${video.thumbnail_link} alt="" />
@@ -79,24 +79,41 @@ function getVideosListInMainPage() {
                     </div>
                     <button class="save_to_playlist_button btn btn-sm btn-outline-primary">Save to my playlist</button>
                 </div>`;
-            if (i < 5) {
+            if (i < page_count + 5 ) {
                 listBodyFirstRow += listBody;
             } else {
                 listBodySecondRow += listBody;
             }
         }
+    }
+
+    return [listBodyFirstRow, listBodySecondRow];
+}
+
+function getVideosListInMainPage() {
+    /**
+     * When user enters the main page, send request to the server and get the list of trending videos.
+     */
+    let url =  getAPIBaseURL();
+
+    fetch(url, {method: 'get'})
+    .then((response) => response.json())
+    .then((videos) => {
+        videos_list = videos;
+        videos_list_total_number = videos_list.length;
+
+        let listBodies = renderHorizontalVideosList(0);
 
         // let videosListElement = document.getElementById('now_trending_videos_list');
         let videosListElementFirstRow = document.getElementById('now_trending_videos_list_1');
         let videosListElementSecondRow = document.getElementById('now_trending_videos_list_2');
         if (videosListElementFirstRow) {
-            videosListElementFirstRow.innerHTML = listBodyFirstRow;
+            videosListElementFirstRow.innerHTML = listBodies[0];
         }
         if (videosListElementSecondRow) {
-            videosListElementSecondRow.innerHTML = listBodySecondRow;
+            videosListElementSecondRow.innerHTML = listBodies[1];
         }
 
-        console.log(videos_list_total_number);
         onLoadVideoPages();
     })
     .catch(function(error) {
@@ -118,38 +135,17 @@ function onSearchButton() {
     fetch(url, {method: 'get'})
     .then((response) => response.json())
     .then((videos) => {
-        let listBodyFirstRow = '';
-        let listBodySecondRow = '';
         videos_list_current_page = 0;
-        for (let i = 0; i < 10; i++) {
-            let video = videos[i];
-            let listBody = `<div class="now_trending_videos_list_item col card">
-                <a href="https://www.youtube.com/watch?v=${video.link}" target="_blank">
-                    <img class="video_img card-img-top" src=${video.thumbnail_link} alt="" />
-                    <div class="video_title">${video.title}</div>
-                </a>
-                    <div class="video_channel">${video.channel}</div>
-                    <div class="video_list_item_text">
-                        <span class="video_views">${video.views} </span>|
-                        <span class="video_likes">${video.likes} </span>|
-                        <span class="video_dislikes">${video.dislikes}</span>
-                    </div>
-                    <button class="save_to_playlist_button btn btn-sm btn-outline-primary">Save to my playlist</button>
-                </div>`;
-            if (i < 5) {
-                listBodyFirstRow += listBody;
-            } else {
-                listBodySecondRow += listBody;
-            }
-        }
+        videos_list = videos;
+        let listBodies = renderHorizontalVideosList(0);
 
         let videosListElementFirstRow = document.getElementById('now_trending_videos_list_1');
         let videosListElementSecondRow = document.getElementById('now_trending_videos_list_2');
         if (videosListElementFirstRow) {
-            videosListElementFirstRow.innerHTML = listBodyFirstRow;
+            videosListElementFirstRow.innerHTML = listBodies[0];
         }
         if (videosListElementSecondRow) {
-            videosListElementSecondRow.innerHTML = listBodySecondRow;
+            videosListElementSecondRow.innerHTML = listBodies[1];
         }
     })
     .catch(function(error) {
@@ -173,35 +169,19 @@ function onPrevVideosButton() {
 
     videos_list_current_page -= 1;
     let prev_page_count = videos_list_current_page * 10;
-    let listBodyFirstRow = '';
-    let listBodySecondRow = '';
-    for (let i = prev_page_count; i < prev_page_count + 10; i++) {
-        let video = videos_list[i];
-        let listBody = `<div class="now_trending_videos_list_item col card">
-            <a href="https://www.youtube.com/watch?v=${video.link}" target="_blank">
-                <img class="video_img card-img-top" src=${video.thumbnail_link} alt="" />
-                <div class="video_title">${video.title}</div>
-            </a>
-                <div class="video_channel">${video.channel}</div>
-                <div class="video_list_item_text">
-                    <span class="video_views">${video.views} </span>|
-                    <span class="video_likes">${video.likes} </span>|
-                    <span class="video_dislikes">${video.dislikes}</span>
-                </div>
-                <button class="save_to_playlist_button btn btn-sm btn-outline-primary">Save to my playlist</button>
-            </div>`;
-        if (i < prev_page_count + 5 ) {
-            listBodyFirstRow += listBody;
-        } else {
-            listBodySecondRow += listBody;
-        }
-    }
+    
+    let currentPageItemElement = document.getElementById(`page-num${videos_list_current_page+2}`);
+    currentPageItemElement.classList.remove("active");
+    currentPageItemElement = document.getElementById(`page-num${videos_list_current_page+1}`);
+    currentPageItemElement.classList.add("active");
+    
+    let listBodies = renderHorizontalVideosList(prev_page_count);
 
     if (videosListElementFirstRow) {
-        videosListElementFirstRow.innerHTML = listBodyFirstRow;
+        videosListElementFirstRow.innerHTML = listBodies[0];
     }
     if (videosListElementSecondRow) {
-        videosListElementSecondRow.innerHTML = listBodySecondRow;
+        videosListElementSecondRow.innerHTML = listBodies[1];
     }
 }
 
@@ -209,50 +189,29 @@ function onNextVideosButton() {
     /**
      * When user clicks next button, render the list of videos that have smaller index number.
      */
-    if (videos_list_current_page == Math.ceil(videos_list.length / 10) - 1) {
+    if (videos_list_current_page == Math.ceil(videos_list_total_number / 10) - 1) {
         alert('This is the last page of videos.');
         return;
     }
 
     let videosListElementFirstRow = document.getElementById('now_trending_videos_list_1');
     let videosListElementSecondRow = document.getElementById('now_trending_videos_list_2');
-    videosListElementFirstRow.innerHTML = '';
-    videosListElementSecondRow.innerHTML = '';
-
     
     videos_list_current_page += 1;
     let next_page_count = videos_list_current_page * 10;
-    let listBodyFirstRow = '';
-    let listBodySecondRow = '';
-    for (let i = next_page_count; i < next_page_count + 10; i++) {
-        if (videos_list[i]) {
-            let video = videos_list[i];
-            let listBody = `<div class="now_trending_videos_list_item col card">
-                <a href="https://www.youtube.com/watch?v=${video.link}" target="_blank">
-                    <img class="video_img card-img-top" src=${video.thumbnail_link} alt="" />
-                    <div class="video_title">${video.title}</div>
-                </a>
-                    <div class="video_channel">${video.channel}</div>
-                    <div class="video_list_item_text">
-                        <span class="video_views">${video.views} </span>|
-                        <span class="video_likes">${video.likes} </span>|
-                        <span class="video_dislikes">${video.dislikes}</span>
-                    </div>
-                    <button class="save_to_playlist_button btn btn-sm btn-outline-primary">Save to my playlist</button>
-                </div>`;
-            if (i < next_page_count + 5 ) {
-                listBodyFirstRow += listBody;
-            } else {
-                listBodySecondRow += listBody;
-            }
-        }
-    }
+    
+    let currentPageItemElement = document.getElementById(`page-num${videos_list_current_page}`);
+    currentPageItemElement.classList.remove("active");
+    currentPageItemElement = document.getElementById(`page-num${videos_list_current_page+1}`);
+    currentPageItemElement.classList.add("active");
+    
+    let listBodies = renderHorizontalVideosList(next_page_count);
 
     if (videosListElementFirstRow) {
-        videosListElementFirstRow.innerHTML = listBodyFirstRow;
+        videosListElementFirstRow.innerHTML = listBodies[0];
     }
     if (videosListElementSecondRow) {
-        videosListElementSecondRow.innerHTML = listBodySecondRow;
+        videosListElementSecondRow.innerHTML = listBodies[1];
     }
 }
 
@@ -261,17 +220,45 @@ function onLoadVideoPages() {
      * Implement the pagination of the video list.
      */
     let verticalVideosListElement = document.getElementById("vertical-videos-list");
-    console.log(videos_list_total_number);
     let videos_list_total_pages = Math.ceil(videos_list_total_number / 10);
-    console.log(videos_list_total_pages)
-    let listBody = '<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1" aria-disabled="true">Prev</a></li>';
-    for (let i = 0; i < videos_list_total_pages; i++) {
-        listBody += `<li class="page-item"><a class="page-link" href="#">${i+1}</a></li>`;
-    }
-    listBody += '<li class="page-item"><a class="page-link" href="#">Next</a></li>';
- 
 
+    // let listBody = '<button type="button" id="prev_button" class="page-item disabled"><a class="page-link" href="#" tabindex="-1" aria-disabled="true">Prev</a></button>';
+    let listBody = '';
+    for (let i = 0; i < videos_list_total_pages; i++) {
+        (i == 0) ? listBody += `<li id="page-num${i+1}" class="page-item active"><a class="page-link" href="#">${i+1}</a></li>`
+        : listBody += `<li id="page-num${i+1}" class="page-item"><a class="page-link" href="#">${i+1}</a></li>`;
+    }
+    // listBody += '<button type="button" id="next_button" class="page-item"><a class="page-link" href="#">Next</a></button>';
+ 
     verticalVideosListElement.innerHTML = listBody;
+}
+
+function onClickVideoPage() {
+    /**
+     * Implement onClick function that navigates to the page clicked and switches the videos.
+     */
+    console.log('hi');
+    let videosListElementFirstRow = document.getElementById('now_trending_videos_list_1');
+    let videosListElementSecondRow = document.getElementById('now_trending_videos_list_2');
+    videosListElementFirstRow.innerHTML = '';
+    videosListElementSecondRow.innerHTML = '';
+
+    videos_list_current_page += 1;
+    let selected_page_count = videos_list_current_page * 10;
+    
+    let currentPageItemElement = document.getElementById(`page-num${videos_list_current_page}`);
+    currentPageItemElement.classList.remove("active");
+    currentPageItemElement = document.getElementById(`page-num${videos_list_current_page+1}`);
+    currentPageItemElement.classList.add("active");
+
+    let listBodies = renderHorizontalVideosList();
+
+    if (videosListElementFirstRow) {
+        videosListElementFirstRow.innerHTML = listBodies[0];
+    }
+    if (videosListElementSecondRow) {
+        videosListElementSecondRow.innerHTML = listBodies[1];
+    }    
 }
 
 function onVerticalViewButton() {
